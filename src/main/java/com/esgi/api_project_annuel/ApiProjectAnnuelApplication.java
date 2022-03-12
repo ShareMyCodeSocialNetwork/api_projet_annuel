414692436f1233f6284885ad9ae8e68e32ad97da
DH7789-dev <dharnaud77@hotmail.fr>
2022-03-12 18:47:46 +0100

    fix

---
+++ src/main/java/com/esgi/api_project_annuel/ApiProjectAnnuelApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.api_project_annuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectAnnuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjectAnnuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esgi/api_project_annuel/ApiProjectAnnuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.api_project_annuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectAnnuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
