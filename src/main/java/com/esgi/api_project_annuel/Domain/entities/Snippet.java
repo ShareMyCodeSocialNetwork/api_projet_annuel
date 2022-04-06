84097274b87d63026a026ed8d53c746c3155a9ab
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-06 15:14:57 +0200

    :rocket: entities

---
+++ src/main/java/com/esgi/api_project_annuel/Domain/entities/Snippet.java
@@ -0,0 +1,27 @@
+package com.esgi.api_project_annuel.Domain.entities;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "snippet_share")
+public class Snippet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private int id;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "language_id")
+    private Language language;
+
+}
