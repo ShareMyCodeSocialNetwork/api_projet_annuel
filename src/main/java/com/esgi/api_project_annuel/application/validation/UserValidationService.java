67c761da6ef295051a4fe08330ad7fd2f40c419e
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-13 00:51:50 +0200

    :sparkles: user :)

--- src/main/java/com/esgi/api_project_annuel/application/validation/UserValidationService.java
+++ src/main/java/com/esgi/api_project_annuel/application/validation/UserValidationService.java
@@ -1,12 +1,6 @@
 package com.esgi.api_project_annuel.application.validation;
 
 import com.esgi.api_project_annuel.Domain.entities.User;
-import com.esgi.api_project_annuel.kernel.logger.Logger;
-import com.esgi.api_project_annuel.kernel.logger.LoggerFactory;
-import org.springframework.util.StringUtils;
-
-import java.util.Objects;
-import java.util.regex.Pattern;
 
 public class UserValidationService {
 
@@ -16,7 +10,7 @@ public class UserValidationService {
 
     public boolean isUserValid(User user) {
 
-        return  !user.getFirstName().isBlank() && !user.getLastName().isBlank() && new EmailValidation().isValid(user.getEmail()) && user.getPassword().length() >= 8 && user.getPassword().length() <= 30;
+        return  !user.getFirstname().isBlank() && !user.getLastname().isBlank() && new EmailValidation().isValid(user.getEmail()) && user.getPassword().length() >= 8 && user.getPassword().length() <= 30;
         //todo : voir pour renvoyer la bonne erreur
     }
 
