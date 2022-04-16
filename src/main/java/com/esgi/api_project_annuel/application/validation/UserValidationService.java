b1615e47a8857eac6eb09f22e2e689816a98bc20
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-16 23:10:21 +0200

    :art: UserCommand

--- src/main/java/com/esgi/api_project_annuel/application/validation/UserValidationService.java
+++ src/main/java/com/esgi/api_project_annuel/application/validation/UserValidationService.java
@@ -9,9 +9,9 @@ public class UserValidationService {
     }
 
     public boolean isUserValid(User user) {
-
+        if(user == null)
+            return false;
         return  !user.getFirstname().isBlank() && !user.getLastname().isBlank() && new EmailValidation().isValid(user.getEmail()) && user.getPassword().length() >= 8 && user.getPassword().length() <= 30;
-        //todo : voir pour renvoyer la bonne erreur
     }
 
 }
