2bec685d0effdfc7ba3439b9e9ff17c8e81838f5
xof <ljehanno@myges.fr>
2022-04-25 23:03:30 +0200

    :art: group command

--- src/main/java/com/esgi/api_project_annuel/application/command/GroupCommand.java
+++ src/main/java/com/esgi/api_project_annuel/application/command/GroupCommand.java
@@ -3,13 +3,11 @@ package com.esgi.api_project_annuel.application.command;
 
 import com.esgi.api_project_annuel.Domain.entities.Group;
 import com.esgi.api_project_annuel.Domain.repository.GroupRepository;
-import com.esgi.api_project_annuel.Domain.repository.UserRepository;
 import com.esgi.api_project_annuel.application.validation.GroupValidationService;
 import com.esgi.api_project_annuel.web.request.GroupRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.InvalidObjectException;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -24,7 +22,7 @@ public class GroupCommand {
 
     public Group create(GroupRequest groupRequest) {
         var group = new Group();
-        group.setGroupName(groupRequest.name);
+        group.setName(groupRequest.name);
         if (!groupValidationService.isValid(group))
             return null;
         return groupRepository.save(group);
@@ -36,9 +34,9 @@ public class GroupCommand {
             return null;
         Optional<Group> groupFromDB = Optional.ofNullable(groupRepository.findById(groupId));
         if(groupFromDB.isPresent()) {
-            groupFromDB.get().setGroupName(groupRequest.name);
+            groupFromDB.get().setName(groupRequest.name);
             if (groupValidationService.isValid(groupFromDB.get()))
-                return groupRepository.save(groupFromDB.get());;
+                return groupRepository.save(groupFromDB.get());
         }
         return null;
     }
