cf747d284d33f850d0f01c7cd3c964b61e6b123a
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-30 22:49:19 +0200

    :rocket: follow validation

--- src/main/java/com/esgi/api_project_annuel/application/validation/FollowValidationService.java
+++ src/main/java/com/esgi/api_project_annuel/application/validation/FollowValidationService.java
@@ -5,6 +5,6 @@ import com.esgi.api_project_annuel.Domain.entities.Follow;
 public class FollowValidationService {
     private final UserValidationService userValidationService = new UserValidationService();
     public boolean isValid(Follow follow){
-        return userValidationService.isUserValid(follow.getFollowedUserId()) && userValidationService.isUserValid(follow.getFollowerUserId());
+        return userValidationService.isUserValid(follow.getFollowedUser()) && userValidationService.isUserValid(follow.getFollowerUser());
     }
 }
