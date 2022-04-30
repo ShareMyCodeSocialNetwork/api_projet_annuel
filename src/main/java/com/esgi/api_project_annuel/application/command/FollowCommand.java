96ae2db8777f1edd7d98497dada1287f4e983c86
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-30 22:48:30 +0200

    :rocket: followcommand

--- src/main/java/com/esgi/api_project_annuel/application/command/FollowCommand.java
+++ src/main/java/com/esgi/api_project_annuel/application/command/FollowCommand.java
@@ -1,8 +1,9 @@
 package com.esgi.api_project_annuel.application.command;
 
 import com.esgi.api_project_annuel.Domain.entities.Follow;
+import com.esgi.api_project_annuel.Domain.entities.User;
 import com.esgi.api_project_annuel.Domain.repository.FollowRepository;
-import com.esgi.api_project_annuel.web.request.FollowRequest;
+import com.esgi.api_project_annuel.application.validation.FollowValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,15 @@ public class FollowCommand {
     @Autowired
     FollowRepository followRepository;
 
-    public Follow create(FollowRequest followRequest){
-        Follow follow = new Follow();
+    FollowValidationService followValidationService = new FollowValidationService();
 
-        return followRepository.save(follow);
+    public Follow create(User followed, User follower){
+        Follow follow = new Follow();
+        follow.setFollowedUser(followed);
+        follow.setFollowerUser(follower);
+        if (followValidationService.isValid(follow))
+            return followRepository.save(follow);
+        return null;
     }
 
 }
