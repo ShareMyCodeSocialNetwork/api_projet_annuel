304cc29af7f8e8315b47a5563f10e322e5b3af19
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-30 22:48:52 +0200

    :rocket: follow repository

--- src/main/java/com/esgi/api_project_annuel/Domain/repository/FollowRepository.java
+++ src/main/java/com/esgi/api_project_annuel/Domain/repository/FollowRepository.java
@@ -1,11 +1,19 @@
 package com.esgi.api_project_annuel.Domain.repository;
 
 import com.esgi.api_project_annuel.Domain.entities.Follow;
+import com.esgi.api_project_annuel.Domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Integer> {
-    Follow findById(long id);
+    Follow findById(int id);
+
+    List<Follow> getAllByFollowedUser(User followed);
+
+    List<Follow> getAllByFollowerUser(User follower);
+
 
 }
