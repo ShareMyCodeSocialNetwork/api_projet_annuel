37ba2dd72cf6a595c48c3df526a16776de4a4f03
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-04 08:16:35 +0200

    :sparkles: UserRoleGroupValidationService

---
+++ src/main/java/com/esgi/api_project_annuel/application/validation/UserRoleGroupValidationService.java
@@ -0,0 +1,16 @@
+package com.esgi.api_project_annuel.application.validation;
+
+import com.esgi.api_project_annuel.Domain.entities.UserRoleGroup;
+
+public class UserRoleGroupValidationService {
+
+    private final UserValidationService userValidationService = new UserValidationService();
+    private final GroupValidationService groupValidationService = new GroupValidationService();
+    private final RoleValidationService roleValidationService = new RoleValidationService();
+
+    public boolean isValid(UserRoleGroup userRoleGroup){
+        return userValidationService.isUserValid(userRoleGroup.getUser()) &&
+                groupValidationService.isValid(userRoleGroup.getGroup()) &&
+                roleValidationService.isValid(userRoleGroup.getRole());
+    }
+}
