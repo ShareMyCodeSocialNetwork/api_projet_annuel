04e31364219227d35e8cd37a483e5832f9ded317
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-09 16:57:56 +0200

    :fire: remove unused import

--- src/main/java/com/esgi/api_project_annuel/application/query/LikeQuery.java
+++ src/main/java/com/esgi/api_project_annuel/application/query/LikeQuery.java
@@ -3,7 +3,6 @@ package com.esgi.api_project_annuel.application.query;
 import com.esgi.api_project_annuel.Domain.entities.Like;
 import com.esgi.api_project_annuel.Domain.entities.Post;
 import com.esgi.api_project_annuel.Domain.repository.LikeRepository;
-import com.esgi.api_project_annuel.Domain.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
