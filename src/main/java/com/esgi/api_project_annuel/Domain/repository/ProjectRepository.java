65210388073a78c1af536b43c32e9ad1c6c306ba
xof <ljehanno@myges.fr>
2022-05-12 18:17:30 +0200

    :rocket: ProjectRepository

---
+++ src/main/java/com/esgi/api_project_annuel/Domain/repository/ProjectRepository.java
@@ -0,0 +1,21 @@
+package com.esgi.api_project_annuel.Domain.repository;
+
+import com.esgi.api_project_annuel.Domain.entities.Group;
+import com.esgi.api_project_annuel.Domain.entities.Project;
+import com.esgi.api_project_annuel.Domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+    Project findById(int id);
+
+    List<Project> getProjectByUser(User user);
+
+    List<Project> findAllByName(String name);
+
+    List<Project> findAllByGroup(Group group);
+}
+
