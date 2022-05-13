c3c75b59468283b919fae0e314ea9711f59e7a89
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-13 20:53:46 +0200

    :rocket: project request

--- src/main/java/com/esgi/api_project_annuel/web/request/ProjectRequest.java
+++ src/main/java/com/esgi/api_project_annuel/web/request/ProjectRequest.java
@@ -3,6 +3,5 @@ package com.esgi.api_project_annuel.web.request;
 public class ProjectRequest {
     public String name;
     public int user_id;
-    public int collection_id;
     public int group_id;
 }
