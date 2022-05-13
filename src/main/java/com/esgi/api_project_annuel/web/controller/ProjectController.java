314b1f6dc51e6dd6f603b3304c605922f23e13db
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-13 20:54:39 +0200

    :rocket: init project controller

---
+++ src/main/java/com/esgi/api_project_annuel/web/controller/ProjectController.java
@@ -0,0 +1,11 @@
+package com.esgi.api_project_annuel.web.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+}
