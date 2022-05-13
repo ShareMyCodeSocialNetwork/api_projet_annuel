40f468863bf73a81dc2b03c7f70ffc376061c7bf
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-13 20:53:34 +0200

    :rocket: project query

---
+++ src/main/java/com/esgi/api_project_annuel/application/query/ProjectQuery.java
@@ -0,0 +1,40 @@
+package com.esgi.api_project_annuel.application.query;
+
+import com.esgi.api_project_annuel.Domain.entities.Group;
+import com.esgi.api_project_annuel.Domain.entities.Project;
+import com.esgi.api_project_annuel.Domain.entities.User;
+import com.esgi.api_project_annuel.Domain.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectQuery {
+    @Autowired
+    ProjectRepository projectRepository;
+
+    public ProjectQuery(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getAll(){
+        return projectRepository.findAll();
+    }
+
+    public Project getById(int id){
+        return projectRepository.findById(id);
+    }
+
+    public List<Project> getByName(String name) {
+        return projectRepository.findAllByName(name);
+    }
+
+    public List<Project> getByUser(User user) {
+        return projectRepository.getProjectByUser(user);
+    }
+
+    public List<Project> getByGroup(Group group) {
+        return projectRepository.findAllByGroup(group);
+    }
+}
