d645b8c948255cde8c9dfb0898eeca42b728b5d8
xof <ljehanno@myges.fr>
2022-05-14 13:12:07 +0200

    :rocket: project validation service

--- src/main/java/com/esgi/api_project_annuel/application/validation/ProjectValidationService.java
+++ src/main/java/com/esgi/api_project_annuel/application/validation/ProjectValidationService.java
@@ -5,17 +5,12 @@ import com.esgi.api_project_annuel.Domain.entities.Project;
 import java.util.Objects;
 
 public class ProjectValidationService {
-
     private final UserValidationService userValidationService = new UserValidationService();
-    private final GroupValidationService groupValidationService = new GroupValidationService();
-
-
     public boolean isValid(Project project){
         if(project == null)
             return false;
 
-        return !userValidationService.isUserValid(project.getUser()) &&
-                !groupValidationService.isValid(project.getGroup()) &&
+        return !userValidationService.isUserValid(project.getOwner()) &&
                 !Objects.equals(project.getName(), "") &&
                 project.getName() != null;
     }
