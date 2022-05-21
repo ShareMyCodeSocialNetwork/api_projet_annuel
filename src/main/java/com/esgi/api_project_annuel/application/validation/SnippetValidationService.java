a50bc85661ce29f6da220eb0519a2f00d3b3ee8d
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-21 06:08:55 +0200

    :sparkles: SnippetValidationService

--- src/main/java/com/esgi/api_project_annuel/application/validation/SnippetValidationService.java
+++ src/main/java/com/esgi/api_project_annuel/application/validation/SnippetValidationService.java
@@ -4,5 +4,15 @@ import com.esgi.api_project_annuel.Domain.entities.Snippet;
 
 public class SnippetValidationService {
 
-    public boolean snippetIsValid(Snippet snippet){return !snippet.getContent().isBlank();}
+    private final LanguageValidationService languageValidationService = new LanguageValidationService();
+    private final UserValidationService userValidationService = new UserValidationService();
+
+    public boolean snippetIsValid(Snippet snippet){
+        return languageValidationService.languageIsValid(snippet.getLanguage()) &&
+                userValidationService.isUserValid(snippet.getUser()) &&
+                !snippet.getContent().equals("") &&
+                !snippet.getContent().isEmpty() &&
+                !snippet.getName().equals("") &&
+                !snippet.getName().isEmpty();
+    }
 }
