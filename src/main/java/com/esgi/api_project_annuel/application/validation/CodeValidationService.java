d7f27d04d391dbfa1c61e98afadc898433a0caf0
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-21 19:42:50 +0200

    :sparkles: CodeValidationService

--- src/main/java/com/esgi/api_project_annuel/application/validation/CodeValidationService.java
+++ src/main/java/com/esgi/api_project_annuel/application/validation/CodeValidationService.java
@@ -4,8 +4,15 @@ import com.esgi.api_project_annuel.Domain.entities.Code;
 
 public class CodeValidationService {
 
+    private final UserValidationService userValidationService = new UserValidationService();
+    private final LanguageValidationService languageValidationService = new LanguageValidationService();
     public boolean codeIsValid(Code code){
-        return !code.getContent().isBlank();
+        return languageValidationService.languageIsValid(code.getLanguage()) &&
+                userValidationService.isUserValid(code.getUser()) &&
+                !code.getContent().equals("") &&
+                !code.getContent().isEmpty() &&
+                !code.getNameCode().equals("") &&
+                !code.getNameCode().isEmpty();
     }
 
 }
