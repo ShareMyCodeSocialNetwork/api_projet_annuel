dc704a9f37d5fedeaeaff4f37bc45de6e369b0d3
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-31 00:02:01 +0200

    :rocket: LanguageValidationServiceTest

---
+++ src/test/java/com/esgi/api_project_annuel/application/validation/LanguageValidationServiceTest.java
@@ -0,0 +1,19 @@
+package com.esgi.api_project_annuel.application.validation;
+
+import com.esgi.api_project_annuel.Domain.entities.Code;
+import com.esgi.api_project_annuel.Domain.entities.Language;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LanguageValidationServiceTest {
+
+    GlobalObject globalObject = new GlobalObject();
+    LanguageValidationService validationService = new LanguageValidationService();
+    @Test
+    void languageIsValid() {
+        assertThat(validationService.languageIsValid(globalObject.validLanguage)).isEqualTo(true);
+        assertThat(validationService.languageIsValid(null)).isEqualTo(false);
+    }
+}
