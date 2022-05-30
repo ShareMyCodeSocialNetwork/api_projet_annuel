6df594e3436af1121c190f7a34bc331afa70d34c
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-30 23:01:50 +0200

    :rocket: test email validation

--- src/test/java/com/esgi/api_project_annuel/application/validation/EmailValidationTest.java
+++ src/test/java/com/esgi/api_project_annuel/application/validation/EmailValidationTest.java
@@ -1,17 +1,16 @@
 package com.esgi.api_project_annuel.application.validation;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
 class EmailValidationTest {
-
-    @BeforeEach
-    void setUp() {
-    }
+    EmailValidation emailValidation= new EmailValidation();
 
     @Test
     void isValid() {
+        var email = "lucas.jehanno@gmail.com";
+        assertThat(emailValidation.isValid(email)).isEqualTo(true);
+        email = "lucas.jehanno@";
+        assertThat(emailValidation.isValid(email)).isEqualTo(false);
     }
 }
