dfef8725422847457bde101cd6476e12912d8dd7
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-30 22:08:54 +0200

    :rocket: follow repo tests

---
+++ src/test/java/com/esgi/api_project_annuel/Domain/repository/FollowRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.esgi.api_project_annuel.Domain.repository;
+
+import com.esgi.api_project_annuel.Domain.entities.Follow;
+import com.esgi.api_project_annuel.Domain.entities.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class FollowRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    FollowRepository repository;
+
+    private final Follow object1 = new Follow();
+    private final Follow object2 = new Follow();
+
+    @BeforeEach
+    void setUp() {
+        object1.setFollowedUser(new User());
+        object1.setFollowerUser(new User());
+        object2.setFollowedUser(new User());
+        object2.setFollowerUser(new User());
+    }
+    @Test
+    public void should_find_empty() {
+        var result = repository.findAll();
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void should_find_no_user_if_repository_is_empty() {
+        var result = repository.findAll();
+        assertThat(result).size().isEqualTo(0);
+    }
+
+    @Test
+    public void should_find_all() {
+
+        entityManager.persist(object1);
+
+        entityManager.persist(object2);
+
+        var results = repository.findAll();
+        assertThat(results).hasSize(2).contains(object1, object2);
+    }
+
+    @Test
+    void findById() {
+        entityManager.persist(object1);
+        var createdObject2 = entityManager.persist(object2);
+
+        var dbContent = repository.findById(createdObject2.getId());
+        assertThat(dbContent.getFollowedUser()).isEqualTo(createdObject2.getFollowedUser());
+        assertThat(dbContent.getId()).isEqualTo(createdObject2.getId());
+        assertThat(dbContent.getFollowerUser()).isEqualTo(createdObject2.getFollowerUser());
+    }
+
+}
