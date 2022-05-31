c62c9a23daf7d62af60a674a2776b3f8b278c1d7
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-31 11:38:49 +0200

    :rocket: ProjectValidationServiceTest

--- src/test/java/com/esgi/api_project_annuel/application/validation/ProjectValidationServiceTest.java
+++ src/test/java/com/esgi/api_project_annuel/application/validation/ProjectValidationServiceTest.java
@@ -1,9 +1,9 @@
 package com.esgi.api_project_annuel.application.validation;
 
+import com.esgi.api_project_annuel.GlobalObject;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectValidationServiceTest {
 
