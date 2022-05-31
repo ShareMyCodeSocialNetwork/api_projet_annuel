79d4c1358901587762a4d909457e056c6e69582a
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-31 15:17:11 +0200

    :white_check_mark: RoleControllerTest

---
+++ src/test/java/com/esgi/api_project_annuel/web/controller/RoleControllerTest.java
@@ -0,0 +1,108 @@
+package com.esgi.api_project_annuel.web.controller;
+
+import com.esgi.api_project_annuel.GlobalObject;
+import com.esgi.api_project_annuel.web.response.RoleResponse;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class RoleControllerTest {
+
+    @LocalServerPort
+    int port;
+    GlobalObject globalObject = new GlobalObject();
+
+    @BeforeEach
+    void setup(){
+        RestAssured.port = port;
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+
+    @Test
+    void addRole() {
+        var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
+        RoleFixture.create(request).then()
+                .statusCode(200)
+                .extract().body().jsonPath().getObject(".", RoleResponse.class);
+        request.name = "";
+        RoleFixture.create(request).then()
+                .statusCode(400);
+
+    }
+
+    @Test
+    void getAll() {
+        var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
+        RoleFixture.create(request).then()
+                .statusCode(200);
+        var response  = RoleFixture.getAll().then()
+                .statusCode(200)
+                .extract().body().jsonPath().getList(".", RoleResponse.class);
+        assertThat(response).isNotEmpty();
+    }
+
+    @Test
+    void getRoleById() {
+        var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
+        var response = RoleFixture.create(request).then()
+                .statusCode(200)
+                .extract().body().jsonPath().getObject(".", RoleResponse.class);
+
+        RoleFixture.getById(response.id).then()
+                .statusCode(200)
+                .extract().body().jsonPath().getObject(".", RoleResponse.class);
+
+        RoleFixture.getById(0).then()
+                .statusCode(400);
+    }
+
+    @Test
+    void getRoleByName() {
+        var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
+        RoleFixture.create(request).then()
+                .statusCode(200);
+        RoleFixture.getByName(request.name).then()
+                .statusCode(200);
+        //RoleFixture.getByName("bad request").then().statusCode(400);//400 pas possible, ca renvoie un liste vide
+    }
+
+    @Test
+    void changeContent() {
+        var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
+        var response = RoleFixture.create(request).then()
+                .statusCode(200)
+                .extract().body().jsonPath().getObject(".", RoleResponse.class);
+        request.name = "NewName";
+        response = RoleFixture.changeName(response.id,request).then()
+                .statusCode(200)
+                .extract().body().jsonPath().getObject(".", RoleResponse.class);
+        assertThat(response.name).isEqualTo(request.name);
+
+        request.name = "";
+         RoleFixture.changeName(response.id,request).then()
+                .statusCode(400);
+    }
+
+    @Test
+    void deleteRole() {
+        var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
+        var response = RoleFixture.create(request).then()
+                .statusCode(200)
+                .extract().body().jsonPath().getObject(".", RoleResponse.class);
+
+        RoleFixture.deleteById(response.id).then()
+                .statusCode(200);
+
+        RoleFixture.deleteById(response.getId()).then()
+                .statusCode(400);
+    }
+}
---
+++ src/test/java/com/esgi/api_project_annuel/web/controller/RoleFixture.java
@@ -0,0 +1,68 @@
+package com.esgi.api_project_annuel.web.controller;
+
+import com.esgi.api_project_annuel.Domain.entities.Role;
+import com.esgi.api_project_annuel.web.request.RoleRequest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class RoleFixture {
+    public static Response create(RoleRequest roleRequest){
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(roleRequest)
+                .post("/role/create");
+    }
+
+    public static Response getById(int id){
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/role/" + id);
+    }
+
+    public static Response getByName(String roleName){
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/role/name/" + roleName);
+    }
+
+
+    public static Response changeName(int id, RoleRequest roleRequest){
+
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(roleRequest)
+                .patch("/role/" + id);
+    }
+
+
+
+    public static Response deleteById(int id){
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("/role/" + id);
+    }
+
+    public static Response getAll(){
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/role/");
+    }
+
+
+
+
+
+    static RoleRequest roleToRoleRequest(Role role){
+        var request = new RoleRequest();
+        request.name = role.getTitlePermission();
+        return request;
+    }
+}
