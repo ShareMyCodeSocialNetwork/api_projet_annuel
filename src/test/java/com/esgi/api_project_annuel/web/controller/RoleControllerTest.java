4a27b7d3def74bca932688de1905314d82006314
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-06-06 17:43:58 +0200

    :white_check_mark: fix role test with token

--- src/test/java/com/esgi/api_project_annuel/web/controller/RoleControllerTest.java
+++ src/test/java/com/esgi/api_project_annuel/web/controller/RoleControllerTest.java
@@ -2,10 +2,12 @@ package com.esgi.api_project_annuel.web.controller;
 
 import com.esgi.api_project_annuel.GlobalObject;
 import com.esgi.api_project_annuel.web.controller.fixture.RoleFixture;
+import com.esgi.api_project_annuel.web.controller.fixture.TokenFixture;
 import com.esgi.api_project_annuel.web.response.RoleResponse;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import org.aspectj.weaver.patterns.IToken;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,11 +32,12 @@ class RoleControllerTest {
     @Test
     void addRole() {
         var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
-        RoleFixture.create(request).then()
+        var token = TokenFixture.adminToken();
+        RoleFixture.create(request,token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", RoleResponse.class);
         request.name = "";
-        RoleFixture.create(request).then()
+        RoleFixture.create(request,token).then()
                 .statusCode(400);
 
     }
@@ -42,9 +45,10 @@ class RoleControllerTest {
     @Test
     void getAll() {
         var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
-        RoleFixture.create(request).then()
+        var token = TokenFixture.adminToken();
+        RoleFixture.create(request,token).then()
                 .statusCode(200);
-        var response  = RoleFixture.getAll().then()
+        var response  = RoleFixture.getAll(token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getList(".", RoleResponse.class);
         assertThat(response).isNotEmpty();
@@ -53,55 +57,59 @@ class RoleControllerTest {
     @Test
     void getRoleById() {
         var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
-        var response = RoleFixture.create(request).then()
+        var token = TokenFixture.adminToken();
+        var response = RoleFixture.create(request, token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", RoleResponse.class);
 
-        RoleFixture.getById(response.id).then()
+        RoleFixture.getById(response.id,token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", RoleResponse.class);
 
-        RoleFixture.getById(0).then()
+        RoleFixture.getById(0,token).then()
                 .statusCode(400);
     }
 
     @Test
     void getRoleByName() {
         var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
-        RoleFixture.create(request).then()
+        var token = TokenFixture.adminToken();
+        RoleFixture.create(request,token).then()
                 .statusCode(200);
-        RoleFixture.getByName(request.name).then()
+        RoleFixture.getByName(request.name,token).then()
                 .statusCode(200);
     }
 
     @Test
-    void changeContent() {
+    void changeTitlePermission() {
+        var token = TokenFixture.adminToken();
         var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
-        var response = RoleFixture.create(request).then()
+        var response = RoleFixture.create(request,token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", RoleResponse.class);
         request.name = "NewName";
-        response = RoleFixture.changeName(response.id,request).then()
+        response = RoleFixture.changeName(response.id,request,token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", RoleResponse.class);
         assertThat(response.name).isEqualTo(request.name);
 
         request.name = "";
-         RoleFixture.changeName(response.id,request).then()
+         RoleFixture.changeName(response.id,request,token).then()
                 .statusCode(400);
     }
 
     @Test
     void deleteRole() {
+        var token = TokenFixture.adminToken();
         var request = RoleFixture.roleToRoleRequest(globalObject.validRole);
-        var response = RoleFixture.create(request).then()
+        var response = RoleFixture.create(request,token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", RoleResponse.class);
 
-        RoleFixture.deleteById(response.id).then()
+        RoleFixture.deleteById(response.id,token).then()
                 .statusCode(200);
 
-        RoleFixture.deleteById(response.getId()).then()
+        RoleFixture.deleteById(response.getId(),token).then()
                 .statusCode(400);
     }
 }
--- src/test/java/com/esgi/api_project_annuel/web/controller/fixture/RoleFixture.java
+++ src/test/java/com/esgi/api_project_annuel/web/controller/fixture/RoleFixture.java
@@ -8,51 +8,57 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 
 public class RoleFixture {
-    public static Response create(RoleRequest roleRequest){
+    public static Response create(RoleRequest roleRequest,Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
                 .body(roleRequest)
+                .header("Authorization","Bearer "+token.access_token)
                 .post("/role/create");
     }
 
-    public static Response getById(int id){
+    public static Response getById(int id,Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .get("/role/" + id);
     }
 
-    public static Response getByName(String roleName){
+    public static Response getByName(String roleName,Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .get("/role/name/" + roleName);
     }
 
 
-    public static Response changeName(int id, RoleRequest roleRequest){
+    public static Response changeName(int id, RoleRequest roleRequest,Token token){
 
         return given()
                 .contentType(ContentType.JSON)
                 .when()
                 .body(roleRequest)
+                .header("Authorization","Bearer "+token.access_token)
                 .patch("/role/" + id);
     }
 
 
 
-    public static Response deleteById(int id){
+    public static Response deleteById(int id,Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .delete("/role/" + id);
     }
 
-    public static Response getAll(){
+    public static Response getAll(Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .get("/role/");
     }
 
