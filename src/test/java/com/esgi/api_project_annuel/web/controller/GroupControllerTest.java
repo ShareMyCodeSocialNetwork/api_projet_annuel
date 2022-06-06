425a6251c2ea248dff579da1c3f69e32b0e6e8db
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-06-06 19:49:17 +0200

    :white_check_mark: group controller tests ok

--- src/test/java/com/esgi/api_project_annuel/web/controller/GroupControllerTest.java
+++ src/test/java/com/esgi/api_project_annuel/web/controller/GroupControllerTest.java
@@ -2,6 +2,7 @@ package com.esgi.api_project_annuel.web.controller;
 
 import com.esgi.api_project_annuel.GlobalObject;
 import com.esgi.api_project_annuel.web.controller.fixture.GroupFixture;
+import com.esgi.api_project_annuel.web.controller.fixture.TokenFixture;
 import com.esgi.api_project_annuel.web.response.GroupResponse;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
@@ -28,103 +29,110 @@ class GroupControllerTest {
     }
     @Test
     void addGroup() {
+        var token = TokenFixture.userToken();
         var request = GroupFixture.groupToGroupRequest(globalObject.validGroup);
-        GroupFixture.create(request).then()
+        GroupFixture.create(request,token).then()
                 .statusCode(201)
                 .extract().body().jsonPath().getObject(".", GroupResponse.class);
         request.name = "";
-        GroupFixture.create(request).then()
+        GroupFixture.create(request,token).then()
                 .statusCode(400);
     }
 
     @Test
     void getGroupAll() {
+        var token = TokenFixture.userToken();
         var request = GroupFixture.groupToGroupRequest(globalObject.validGroup);
-        GroupFixture.create(request).then()
+        GroupFixture.create(request,token).then()
                 .statusCode(201);
-        var response  = GroupFixture.getAll().then()
+        var response  = GroupFixture.getAll(token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getList(".", GroupResponse.class);
         assertThat(response).isNotEmpty();
     }
 
     @Test
-    void getRoleByName() {
+    void getGroupByName() {
+        var token = TokenFixture.userToken();
         var request = GroupFixture.groupToGroupRequest(globalObject.validGroup);
-        GroupFixture.create(request).then()
+        GroupFixture.create(request,token).then()
                 .statusCode(201);
-        GroupFixture.getByName(request.name).then()
+        GroupFixture.getByName(request.name,token).then()
                 .statusCode(200);
     }
 
     @Test
     void getGroupById() {
+        var token = TokenFixture.userToken();
         var request = GroupFixture.groupToGroupRequest(globalObject.validGroup);
-        var response = GroupFixture.create(request).then()
+        var response = GroupFixture.create(request,token).then()
                 .statusCode(201)
                 .extract().body().jsonPath().getObject(".", GroupResponse.class);
 
-        GroupFixture.getById(response.id).then()
+        GroupFixture.getById(response.id,token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", GroupResponse.class);
 
-        GroupFixture.getById(0).then()
+        GroupFixture.getById(0,token).then()
                 .statusCode(404);
     }
 
     @Test
     void changeName() {
+        var token = TokenFixture.userToken();
         var request = GroupFixture.groupToGroupRequest(globalObject.validGroup);
-        var response = GroupFixture.create(request).then()
+        var response = GroupFixture.create(request,token).then()
                 .statusCode(201)
                 .extract().body().jsonPath().getObject(".", GroupResponse.class);
 
-        GroupFixture.changeName(0,request).then()
+        GroupFixture.changeName(0,request,token).then()
                 .statusCode(404);
 
         request.name = "NewName";
-        response = GroupFixture.changeName(response.id,request).then()
+        response = GroupFixture.changeName(response.id,request,token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", GroupResponse.class);
         assertThat(response.name).isEqualTo(request.name);
 
         request.name = "";
-        GroupFixture.changeName(response.id,request).then()
+        GroupFixture.changeName(response.id,request,token).then()
                 .statusCode(400);
     }
 
     @Test
     void updateGroup() {
+        var token = TokenFixture.userToken();
         var request = GroupFixture.groupToGroupRequest(globalObject.validGroup);
-        var response = GroupFixture.create(request).then()
+        var response = GroupFixture.create(request,token).then()
                 .statusCode(201)
                 .extract().body().jsonPath().getObject(".", GroupResponse.class);
 
-        GroupFixture.update(0,request).then()
+        GroupFixture.update(0,request,token).then()
                 .statusCode(400);
 
         request.name = "NewName";
-        response = GroupFixture.update(response.id,request).then()
+        response = GroupFixture.update(response.id,request,token).then()
                 .statusCode(200)
                 .extract().body().jsonPath().getObject(".", GroupResponse.class);
         assertThat(response.name).isEqualTo(request.name);
 
         request.name = "";
-        GroupFixture.changeName(response.id,request).then()
+        GroupFixture.changeName(response.id,request,token).then()
                 .statusCode(400);
     }
 
     @Test
     void deleteGroup() {
+        var token = TokenFixture.userToken();
         var request = GroupFixture.groupToGroupRequest(globalObject.validGroup);
-        var response = GroupFixture.create(request).then()
+        var response = GroupFixture.create(request,token).then()
                 .statusCode(201)
                 .extract().body().jsonPath().getObject(".", GroupResponse.class);
 
-        GroupFixture.deleteById(response.id).then()
+        GroupFixture.deleteById(response.id,token).then()
                 .statusCode(204);
 
-        GroupFixture.deleteById(response.getId()).then()
+        GroupFixture.deleteById(response.getId(),token).then()
                 .statusCode(400);
     }
 }
--- src/test/java/com/esgi/api_project_annuel/web/controller/fixture/GroupFixture.java
+++ src/test/java/com/esgi/api_project_annuel/web/controller/fixture/GroupFixture.java
@@ -9,51 +9,70 @@ import org.assertj.core.api.Java6BDDSoftAssertionsProvider;
 import static io.restassured.RestAssured.given;
 
 public class GroupFixture {
-    public static Response create(GroupRequest groupRequest){
+    public static Response create(GroupRequest groupRequest,Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
                 .body(groupRequest)
+                .header("Authorization","Bearer "+token.access_token)
                 .post("/group/create");
     }
 
-    public static Response getById(int id){
+    public static Response getById(int id,Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .get("/group/" + id);
     }
 
 
 
-    public static Response update(int id, GroupRequest groupRequest){
+    public static Response update(int id, GroupRequest groupRequest,Token token){
 
         return given()
                 .contentType(ContentType.JSON)
                 .when()
                 .body(groupRequest)
+                .header("Authorization","Bearer "+token.access_token)
                 .put("/group/update/" + id);
     }
 
 
 
-    public static Response deleteById(int id){
+    public static Response deleteById(int id,Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
-                .delete("/group/" + id);
+                .header("Authorization","Bearer "+token.access_token)
+                .delete("/group/delete/" + id);
     }
 
-    public static Response getAll(){
+    public static Response getAll(Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .get("/group/");
     }
 
+    public static Response changeName(int id, GroupRequest request,Token token) {
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(request)
+                .header("Authorization","Bearer "+token.access_token)
+                .patch("/group/update/name/" + id);
+    }
 
+    public static Response getByName(String groupName, Token token) {
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .header("Authorization","Bearer "+token.access_token)
+                .get("/group/name/" + groupName);
 
-
+    }
 
     public static GroupRequest groupToGroupRequest(Group group){
         var request = new GroupRequest();
@@ -61,19 +80,7 @@ public class GroupFixture {
         return request;
     }
 
-    public static Response getByName(String groupName) {
-            return given()
-                    .contentType(ContentType.JSON)
-                    .when()
-                    .get("/group/name/" + groupName);
 
-    }
 
-    public static Response changeName(int id, GroupRequest request) {
-        return given()
-                .contentType(ContentType.JSON)
-                .when()
-                .body(request)
-                .patch("/group/change_name/" + id);
-    }
+
 }
